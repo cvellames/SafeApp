7fd7c712a94a7f0cc71ae70b4fbbeb52125afe8d
Cassiano Vellames <cassiano.vellames@topos.local>
2017-08-02 09:54:05 -0300

    config database and scripts for continuous integration

--- config/database.js
+++ config/database.js
@@ -24,6 +24,18 @@ module.exports = {
                 underscored: true
             }
         }
+    },
+    ci : {
+        database: "xtnih0ydn0ola6ju",
+        username: "k4iy6n9ko492m6fp",
+        password: "hbgg09wcom3dafze",
+        params: {
+            logging: true,
+            host: "op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+            dialect: "mysql",
+            define: {
+                underscored: true
+            }
+        }
     }
-
 };
